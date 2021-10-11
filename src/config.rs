f99ae8f4e675abc4da3f93088ab76ece7929f452
Ninad Bhangui <b.ninad50@gmail.com>
2021-10-11 09:07:37 +0530

    cargp clippy

--- src/config.rs
+++ src/config.rs
@@ -11,10 +11,8 @@ impl Config {
             return Err("not enough arguments");
         }
         Ok(Config {
-            api_key: env::var("STEAM_API_KEY")
-                .or_else(|_| Err("STEAM_API_KEY not found in environment"))?,
-            export_path: env::var("EXPORT_PATH")
-                .or_else(|_| Err("EXPORT_PATH not found in environment"))?,
+            api_key: env::var("STEAM_API_KEY").map_err(|_| "STEAM_API_KEY not found in environment")?,
+            export_path: env::var("STEAM_API_KEY").map_err(|_| "STEAM_API_KEY not found in environment")?,
             steamid: args[1].clone(),
         })
     }
